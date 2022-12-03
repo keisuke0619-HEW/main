f8c50e1aaab5c4bcb57df5891e6ed75beb25b0a4
ShimizuKeisuke <nhs10194keisuke@gmail.com>
2022-12-03 23:46:01 +0900

    いーじんぐおねがいしま～す

---
+++ HewProject/HewProject/Source/System/Easing.cpp
@@ -0,0 +1,155 @@
+#include "Easing.hpp"
+#include <math.h>
+#include <macro.hpp>
+
+// https://easings.net/ja
+
+float Easing::InSine(float x)
+{
+    return 1.0f - cosf((x * PI) / 2);
+}
+
+float Easing::OutSine(float x)
+{
+    return sinf((x * PI) / 2);
+}
+
+float Easing::InOutSine(float x)
+{
+    return 0.0f;
+}
+
+float Easing::InCubic(float x)
+{
+    return 0.0f;
+}
+
+float Easing::OutCubic(float x)
+{
+    return 0.0f;
+}
+
+float Easing::InOutCubic(float x)
+{
+    return 0.0f;
+}
+
+float Easing::InQuint(float x)
+{
+    return 0.0f;
+}
+
+float Easing::OutQuint(float x)
+{
+    return 0.0f;
+}
+
+float Easing::InOutQuint(float x)
+{
+    return 0.0f;
+}
+
+float Easing::InCirc(float x)
+{
+    return 0.0f;
+}
+
+float Easing::OutCirc(float x)
+{
+    return 0.0f;
+}
+
+float Easing::InOutCirc(float x)
+{
+    return 0.0f;
+}
+
+float Easing::InQuad(float level)
+{
+    return 0.0f;
+}
+
+float Easing::OutQuad(float level)
+{
+    return 0.0f;
+}
+
+float Easing::InOutQuad(float level)
+{
+    return 0.0f;
+}
+
+float Easing::InQuart(float level)
+{
+    return 0.0f;
+}
+
+float Easing::OutQuart(float level)
+{
+    return 0.0f;
+}
+
+float Easing::InOutQuart(float level)
+{
+    return 0.0f;
+}
+
+float Easing::InExpo(float level)
+{
+    return 0.0f;
+}
+
+float Easing::OutExpo(float level)
+{
+    return 0.0f;
+}
+
+float Easing::InOutExpo(float level)
+{
+    return 0.0f;
+}
+
+float Easing::InBack(float level)
+{
+    return 0.0f;
+}
+
+float Easing::OutBack(float level)
+{
+    return 0.0f;
+}
+
+float Easing::InOutBack(float level)
+{
+    return 0.0f;
+}
+
+float Easing::InElastic(float level)
+{
+    return 0.0f;
+}
+
+float Easing::OutElastic(float level)
+{
+    return 0.0f;
+}
+
+float Easing::InOutElastic(float level)
+{
+    return 0.0f;
+}
+
+float Easing::InBounce(float level)
+{
+    return 0.0f;
+}
+
+float Easing::OutBounce(float level)
+{
+    return 0.0f;
+}
+
+float Easing::InOutBounce(float level)
+{
+    return 0.0f;
+}
---
+++ HewProject/HewProject/Source/System/Easing.hpp
@@ -0,0 +1,39 @@
+#pragma once
+
+namespace Easing
+{
+	 
+	float InSine(float level);
+	float OutSine(float level);
+	float InOutSine(float level);
+	float InCubic(float level);
+	float OutCubic(float level);
+	float InOutCubic(float level);
+	float InQuint(float level);
+	float OutQuint(float level);
+	float InOutQuint(float level);
+	float InCirc(float level);
+	float OutCirc(float level);
+	float InOutCirc(float level);
+	float InQuad(float level);
+	float OutQuad(float level);
+	float InOutQuad(float level);
+	float InQuart(float level);
+	float OutQuart(float level);
+	float InOutQuart(float level);
+	float InExpo(float level);
+	float OutExpo(float level);
+	float InOutExpo(float level);
+	float InBack(float level);
+	float OutBack(float level);
+	float InOutBack(float level);
+	float InElastic(float level);
+	float OutElastic(float level);
+	float InOutElastic(float level);
+	float InBounce(float level);
+	float OutBounce(float level);
+	float InOutBounce(float level);
+
+
+
+}
