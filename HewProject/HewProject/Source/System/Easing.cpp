f6e229d26754d5c4d5081b3074362038db62db45
WNHS10341\佐藤　絢太 <kenta.sib1028@gmail.com>
2022-12-04 00:38:59 +0900

    いーじんぐ

--- HewProject/HewProject/Source/System/Easing.cpp
+++ HewProject/HewProject/Source/System/Easing.cpp
@@ -21,135 +21,182 @@ float Easing::InOutSine(float x)
 
 float Easing::InCubic(float x)
 {
-    return 0.0f;
+    return  x * x * x;
 }
 
 float Easing::OutCubic(float x)
 {
-    return 0.0f;
+    return  1 - powf(1 - x, 3);
 }
 
 float Easing::InOutCubic(float x)
 {
-    return 0.0f;
+    return x < 0.5f ? 4 * x * x * x : 1 - powf(-2 * x + 2, 3) / 2;
 }
 
 float Easing::InQuint(float x)
 {
-    return 0.0f;
+    return  x * x * x * x * x;
 }
 
 float Easing::OutQuint(float x)
 {
-    return 0.0f;
+    return 1 - powf(1 - x, 5);
 }
 
 float Easing::InOutQuint(float x)
 {
-    return 0.0f;
+    return x < 0.5f ? 16 * x * x * x * x * x : 1 - powf(-2 * x + 2, 5) / 2;
 }
 
 float Easing::InCirc(float x)
 {
-    return 0.0f;
+    return 1 - sqrtf(1 - powf(x, 2));
 }
 
 float Easing::OutCirc(float x)
 {
-    return 0.0f;
+    return sqrtf(1 - powf(x - 1, 2));
 }
 
 float Easing::InOutCirc(float x)
 {
-    return 0.0f;
+    return x < 0.5f
+		? (1 - sqrtf(1 - powf(2 * x, 2))) / 2
+		: (sqrtf(1 - powf(-2 * x + 2, 2)) + 1) / 2;
 }
 
-float Easing::InQuad(float level)
+float Easing::InQuad(float x)
 {
-    return 0.0f;
+    return x * x;
 }
 
-float Easing::OutQuad(float level)
+float Easing::OutQuad(float x)
 {
-    return 0.0f;
+    return  1 - (1 - x) * (1 - x);
 }
 
-float Easing::InOutQuad(float level)
+float Easing::InOutQuad(float x)
 {
-    return 0.0f;
+    return x < 0.5f ? 2 * x * x : 1 - powf(-2 * x + 2, 2) / 2;
 }
 
-float Easing::InQuart(float level)
+float Easing::InQuart(float x)
 {
-    return 0.0f;
+    return x * x * x * x;
 }
 
-float Easing::OutQuart(float level)
+float Easing::OutQuart(float x)
 {
-    return 0.0f;
+    return  1 - powf(1 - x, 4);
 }
 
-float Easing::InOutQuart(float level)
+float Easing::InOutQuart(float x)
 {
-    return 0.0f;
+    return x < 0.5f ? 8 * x * x * x * x : 1 - powf(-2 * x + 2, 4) / 2;
 }
 
-float Easing::InExpo(float level)
+float Easing::InExpo(float x)
 {
-    return 0.0f;
+    return  x == 0 ? 0 : powf(2, 10 * x - 10);
 }
 
-float Easing::OutExpo(float level)
+float Easing::OutExpo(float x)
 {
-    return 0.0f;
+    return x ==  1 ? 1 : 1 - powf(2, -10 * x);
 }
 
-float Easing::InOutExpo(float level)
+float Easing::InOutExpo(float x)
 {
-    return 0.0f;
+	return x == 0 ? 0 : x == 1 ? 1 
+			: x < 0.5f ? powf(2, 20 * x - 10) / 2 
+			: (2 - powf(2, -20 * x + 10)) / 2;
 }
 
-float Easing::InBack(float level)
+float Easing::InBack(float x)
 {
-    return 0.0f;
+	const float c1 = 1.70158f;
+	const float c3 = c1 + 1;
+
+	return c3 * x * x * x - c1 * x * x;
 }
 
-float Easing::OutBack(float level)
+float Easing::OutBack(float x)
 {
-    return 0.0f;
+	const float c1 = 1.70158f;
+	const float c3 = c1 + 1;
+
+	return 1 + c3 * powf(x - 1, 3) + c1 * powf(x - 1, 2);
 }
 
-float Easing::InOutBack(float level)
+float Easing::InOutBack(float x)
 {
-    return 0.0f;
+	const float c1 = 1.70158f;
+	const float c2 = c1 * 1.525f;
+
+    return x < 0.5
+		? (powf(2 * x, 2) * ((c2 + 1) * 2 * x - c2)) / 2
+		: (powf(2 * x - 2, 2) * ((c2 + 1) * (x * 2 - 2) + c2) + 2) / 2;
 }
 
-float Easing::InElastic(float level)
+float Easing::InElastic(float x)
 {
-    return 0.0f;
+	const float c4 = (2 * PI) / 3;
+
+	return x == 0 ? 0
+		: x == 1 ? 1
+		: -powf(2, 10 * x - 10) * sinf((x * 10 - 10.75f) * c4);
 }
 
-float Easing::OutElastic(float level)
+float Easing::OutElastic(float x)
 {
-    return 0.0f;
+	const float c4 = (2 * PI) / 3;
+
+	return x == 0 ? 0
+		: x == 1 ? 1
+		: powf(2, -10 * x) * sinf((x * 10 - 0.75f) * c4) + 1;
 }
 
-float Easing::InOutElastic(float level)
+float Easing::InOutElastic(float x)
 {
-    return 0.0f;
+	const float c5 = (2 * PI) / 4.5f;
+
+	return x ==  0
+		? 0
+		: x ==  1
+		? 1
+		: x < 0.5f
+		? -(powf(2, 20 * x - 10) * sinf((20 * x - 11.125f) * c5)) / 2
+		: (powf(2, -20 * x + 10) * sinf((20 * x - 11.125f) * c5)) / 2 + 1;
 }
 
-float Easing::InBounce(float level)
+float Easing::InBounce(float x)
 {
-    return 0.0f;
+	return 1 - OutBounce(1 - x);
 }
 
-float Easing::OutBounce(float level)
+float Easing::OutBounce(float x)
 {
-    return 0.0f;
+	const float n1 = 7.5625f;
+	const float d1 = 2.75f;
+
+	if (x < 1 / d1) {
+		return n1 * x * x;
+	}
+	else if (x < 2 / d1) {
+		return n1 * (x -= 1.5f / d1) * x + 0.75f;
+	}
+	else if (x < 2.5f / d1) {
+		return n1 * (x -= 2.25f / d1) * x + 0.9375f;
+	}
+	else {
+		return n1 * (x -= 2.625f / d1) * x + 0.984375f;
+	}
 }
 
-float Easing::InOutBounce(float level)
+float Easing::InOutBounce(float x)
 {
-    return 0.0f;
+	return x < 0.5f
+		? (1 - OutBounce(1 - 2 * x)) / 2
+		: (1 + OutBounce(2 * x - 1)) / 2;
 }
