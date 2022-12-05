baf7158d043e4ca6a03b690d3ef317323e9d2d23
ShimizuKeisuke <nhs10194keisuke@gmail.com>
2022-12-06 01:27:43 +0900

    直しました

--- HewProject/HewProject/Source/System/BaseClass/IObjectBase.hpp
+++ HewProject/HewProject/Source/System/BaseClass/IObjectBase.hpp
@@ -1,6 +1,5 @@
 #pragma once
 #include <DirectXMath.h>
-#include <CollisionBase.hpp>
 #include <memory>
 #include <string>
 enum EObjectTag
@@ -12,6 +11,13 @@ enum EObjectTag
 	// 必要な場合はここにタグを追記する
 };
 
+enum ECollisionType
+{
+	COLLISION_NONE,		// 当たり判定を使用しない
+	COLLISION_BOX,		// 箱の当たり判定
+	COLLISION_SPHIRE,	// 球の当たり判定
+	// 新たに当たり判定の種類を作る場合はここに追加する。
+};
 
 struct TObjectParam
 {
--- HewProject/HewProject/Source/System/CollisionBase.hpp
+++ HewProject/HewProject/Source/System/CollisionBase.hpp
@@ -1,14 +1,6 @@
 #pragma once
 #include <IObjectBase.hpp>
 
-enum ECollisionType
-{
-	COLLISION_NONE,		// 当たり判定を使用しない
-	COLLISION_BOX,		// 箱の当たり判定
-	COLLISION_SPHIRE,	// 球の当たり判定
-	// 新たに当たり判定の種類を作る場合はここに追加する。
-};
-
 namespace Utility
 {
 	/// <summary>
