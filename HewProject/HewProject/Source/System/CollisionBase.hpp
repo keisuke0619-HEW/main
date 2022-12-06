269764df35ba390d3a9e56b2366c8c535e0545fa
ShimizuKeisuke <nhs10194keisuke@gmail.com>
2022-12-07 01:45:34 +0900

    球の当たり判定あざざます

--- HewProject/HewProject/Source/System/CollisionBase.hpp
+++ HewProject/HewProject/Source/System/CollisionBase.hpp
@@ -10,7 +10,18 @@ namespace Utility
 	/// <param name="obj2">オブジェクト２のデータ</param>
 	/// <returns>当たっていたらTrue・当たっていなかったらFalse</returns>
 	bool IsCollision(TObjectParam obj1, TObjectParam obj2);
-	bool IsCollisionCircle(TObjectParam obj1, TObjectParam obj2, float Rad1, float Rad2);
-	bool IsCollisionBox(TObjectParam obj1, TObjectParam obj2, TObjectParam objSize1, TObjectParam objSize2);
+	
+	/// <summary>
+	/// オブジェクト１と２が衝突していたらTrueを返す
+	/// </summary>
+	/// <param name="obj1">オブジェクト１のデータ</param>
+	/// <param name="obj2">オブジェクト２のデータ</param>
+	/// <returns>当たっていたらTrue・当たっていなかったらFalse</returns>
+	bool IsCollisionShpire(TObjectParam obj1, TObjectParam obj2);
+
+
+
+	//bool IsCollisionCircle(TObjectParam obj1, TObjectParam obj2, float Rad1, float Rad2);
+	//bool IsCollisionBox(TObjectParam obj1, TObjectParam obj2, TObjectParam objSize1, TObjectParam objSize2);
 
 }
