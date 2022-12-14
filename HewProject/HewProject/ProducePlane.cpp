6cac525e29f3acdc64d867bb3acbca1926221044
WNHS10143\konpa <konpakuyoumu0505@gmail.com>
2022-12-15 02:21:01 +0900

    難しかったので明日のPL中にけいすけニキに聞きます。。。お手数をかけますがお願いします

--- HewProject/HewProject/ProducePlane.cpp
+++ HewProject/HewProject/ProducePlane.cpp
@@ -1,14 +1,41 @@
 #include "ProducePlane.h"
-
 void ProducePlane::SetRect(DirectX::XMFLOAT3 pos1, DirectX::XMFLOAT3 pos2)
 {
-
+	m_pPlane->planeVtx[0] = {	};
+	m_pPlane->planeVtx[1] = {	};
+	m_pPlane->planeVtx[2] = {	};
+	m_pPlane->planeVtx[3] = {	};
 }
 
 void ProducePlane::SetCenterPos(DirectX::XMFLOAT3 center, DirectX::XMFLOAT2 size, DirectX::XMFLOAT3 rot)
 {
+	//m_pPlane = new Plane;
+
+	////	中心点などを指定して描画
+	//m_pPlane->centerpos = center;
+	//m_pPlane->size = size;
+	//m_pPlane->rot = rot;
+
+
+
+	//->Draw();
 }
 
 void ProducePlane::SetEnable(bool isDraw)
 {
+	if (isDraw)
+	{
+		//	描画のセットする
+
+
+
+
+
+
+		//->Draw();
+	}
+	else
+	{
+		return;
+	}
 }
--- HewProject/HewProject/ProducePlane.h
+++ HewProject/HewProject/ProducePlane.h
@@ -1,13 +1,23 @@
 #pragma once
-#include <vector>
+#include "ObjectBase.hpp"
 #include "DirectXTex/DirectXTex.h"
 
 class ProducePlane
 {
 public:
+	struct Plane
+	{
+		DirectX::XMFLOAT3 pos;
+		DirectX::XMFLOAT3 centerpos;
+		DirectX::XMFLOAT3 rot;
+		DirectX::XMFLOAT2 size;
+	};
 	void SetRect(DirectX::XMFLOAT3 pos1, DirectX::XMFLOAT3 pos2);//矩形を選択して四角形を設定
 	void SetCenterPos(DirectX::XMFLOAT3 center, DirectX::XMFLOAT2 size, DirectX::XMFLOAT3 rot);// 中心点などを指定して描画
 	void SetEnable(bool isDraw);// 描画するか
 
 private:
+//	Plane* m_pPlane;
+	PlaneData* m_pPlane;
+	bool m_IsDarw;
 };
