68e787b87ed50277b2be3c041d3ff48619964f45
WNHS10143\konpa <konpakuyoumu0505@gmail.com>
2022-12-15 10:21:17 +0900

    非力でごめんなさい

--- HewProject/HewProject/ProducePlane.h
+++ HewProject/HewProject/ProducePlane.h
@@ -1,23 +1,38 @@
 #pragma once
-#include "ObjectBase.hpp"
 #include "DirectXTex/DirectXTex.h"
+#include <Geometory.h>
+#include <ConstantBuffer.h>
 
 class ProducePlane
 {
 public:
-	struct Plane
+	ProducePlane();
+	~ProducePlane();
+	struct PlaneVertex
 	{
-		DirectX::XMFLOAT3 pos;
-		DirectX::XMFLOAT3 centerpos;
-		DirectX::XMFLOAT3 rot;
-		DirectX::XMFLOAT2 size;
+		float pos[3];
+		float uv[2];
+		float size[2];
 	};
-	void SetRect(DirectX::XMFLOAT3 pos1, DirectX::XMFLOAT3 pos2);//矩形を選択して四角形を設定
+	struct UVPos
+	{
+		DirectX::XMFLOAT4 pos[4];
+		float luPosX;
+		float luPosY;
+		float width;
+		float height;
+	};
+	void Draw();
+	void SetRect(DirectX::XMFLOAT3 pos1, DirectX::XMFLOAT3 pos2, 
+		DirectX::XMFLOAT3 pos3, DirectX::XMFLOAT3 pos4);//矩形を選択して四角形を設定
 	void SetCenterPos(DirectX::XMFLOAT3 center, DirectX::XMFLOAT2 size, DirectX::XMFLOAT3 rot);// 中心点などを指定して描画
 	void SetEnable(bool isDraw);// 描画するか
 
 private:
-//	Plane* m_pPlane;
-	PlaneData* m_pPlane;
+	MeshBuffer* m_pProducePlane;
+	ConstantBuffer* m_pConstant;
+	ConstantBuffer* m_pConstantWVP;
+	DirectX::XMFLOAT4X4 m_PlaneMat[3];
+//	DirectX::XMFLOAT3 m_PlaneTransform[3];
 	bool m_IsDarw;
 };
