e356eacb45aac64d9be15c61cdd83d82202d9fbd
平野祐貴 <yuukihirano@i.softbank.jp>
2022-12-24 00:05:33 +0900

    タイトルシーンに画像を挿入

--- HewProject/HewProject/Source/Scene/SceneTitle.cpp
+++ HewProject/HewProject/Source/Scene/SceneTitle.cpp
@@ -2,10 +2,16 @@
 #include <SceneManager.hpp>
 #include <DebugCamera.hpp>
 #include <Controller.hpp>
+#include <UiManager.hpp>
+
 CSceneTitle::CSceneTitle()
 {
 	CCameraBase::CreateCamera(new CDebugCamera(), "Debug");
 	CCameraBase::SetPrimaryCamera("Debug");
+
+	m_TitleUI = CUIManager::GetIns()->Add(new CGameUI("Assets/Img/titleSceneUI.png"));
+	m_TitleUI.lock()->SetSize({ 1200.0f, 600.0f });
+	m_TitleUI.lock()->SetPos({ 640.0f, 360.0f });
 }
 
 CSceneTitle::~CSceneTitle()
@@ -14,6 +20,8 @@ CSceneTitle::~CSceneTitle()
 
 void CSceneTitle::Update()
 {
+	m_TitleUI.lock()->Draw();
+
 	if (Utility::GetKeyTrigger(Key_RETURN))
 	{
 		CSceneManager::SetScene(SCENE_STAGE01);
