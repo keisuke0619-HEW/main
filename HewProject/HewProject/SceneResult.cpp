843b170dc1bca4b7d5bd3af5fc5046cd5e1b9864
佐藤駿磨 <shunma0123@softbank.ne.jp>
2023-01-20 13:46:17 +0900

    リザルト画面の画像差し替え 仮でゲームクリアにしてある 画質やばい

--- HewProject/HewProject/SceneResult.cpp
+++ HewProject/HewProject/SceneResult.cpp
@@ -5,7 +5,7 @@
 #include <UiManager.hpp>
 CSceneResult::CSceneResult()
 {
-	m_ResultUI = CUIManager::GetIns()->Add(new CGameUI("Assets/Img/resultSceneUI.png"));
+	m_ResultUI = CUIManager::GetIns()->Add(new CGameUI("Assets/Img/Result/Gameclear_UI.png"));
 	m_ResultUI.lock()->SetSize({ 1200.f, 600.f });
 	m_ResultUI.lock()->SetPos({ 640.f, 360.f });
 	// ‰¹ƒf[ƒ^‚Ì“Ç‚İ‚İ
