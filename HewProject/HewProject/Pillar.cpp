d6795d604ded50d6e22463501e2469f4d917eb87
彦坂蒼斗 <aoto.220284@outlook.jp>
2023-01-23 22:16:56 +0900

    柱調整完了

--- HewProject/HewProject/Pillar.cpp
+++ HewProject/HewProject/Pillar.cpp
@@ -1,7 +1,7 @@
 #include "Pillar.hpp"
 
 CPillar::CPillar(float x, float y, float z)
-	: CObjectBase("Assets/Model/hasira.fbx", 0.2f)
+	: CObjectBase("Assets/Model/hasira.fbx", 0.3f)
 {
 	m_param.pos.x = x;
 	m_param.pos.y = y;
@@ -10,7 +10,7 @@ CPillar::CPillar(float x, float y, float z)
 	m_param.collisionType = COLLISION_OBSTACLES;
 	m_param.tag = TAG_STATIC_OBJECT;
 	m_param.collisionData.obstacles.pos = m_param.pos;
-	m_param.collisionData.obstacles.size = { 4.0f,4.0f };
+	m_param.collisionData.obstacles.size = { 6.f,6.f };
 	m_param.collisionData.obstacles.spin = false;
 
 	//m_param.drawOffset = { 0,5.8f,0 };
