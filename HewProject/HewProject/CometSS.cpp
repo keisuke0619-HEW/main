8dc79eabb706b53d49136e759e190cea492b7985
清水圭亮 <nhs10194keisuke@gmail.com>
2023-01-31 18:24:47 +0900

    とりあえず流れます

--- HewProject/HewProject/CometSS.cpp
+++ HewProject/HewProject/CometSS.cpp
@@ -2,8 +2,8 @@
 
 CCometSS::CCometSS()
 {
-	m_pos.x = 300;
-	m_pos.y = 300.0f;
+	m_pos.x =  10000 + (rand() % 1880);
+	m_pos.y = -10000;
 	m_handle = CUIManager::GetIns()->Add(new CGameUI("Assets/Img/Title/CometSS.png"), SORT_ORDER_BACK);
 	m_handle.lock()->SetSize(200, 200);
 	m_handle.lock()->SetPos(m_pos);
@@ -15,14 +15,11 @@ CCometSS::~CCometSS()
 
 void CCometSS::Update()
 {
-	//m_pos.x -= 1.0f;
-	//m_pos.y -= 1.0f;
-	if (m_pos.x < -100)
-	{
-		m_pos.x = 1380;
-	}
+	m_pos.x -= 17.0f;
+	m_pos.y += 17.0f;
 	if (m_pos.y > 920)
 	{
+		m_pos.x = 10000 + (rand() % 1880);
 		m_pos.y = -10000;
 	}
 }
